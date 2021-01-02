3565638ad757debc6550e847e3a14609a16df4dd
McKay Ransom <mckay.ransom@gmail.com>
2021-01-02 01:24:58 -0700

    OOP sucks maybe. But still not working

--- python/day4.py
+++ python/day4.py
@@ -1,12 +1,11 @@
 import string
-from typing import Dict, Any
 
 
 def parse_single_passport(passport_info):
-    return Passport({
+    return {
         info.split(":")[0]: info.split(":")[1]
         for info in passport_info.split(" ")
-    })
+    }
 
 
 def parse_passports(file_name):
@@ -21,12 +20,7 @@ def parse_passports(file_name):
         ]
 
 
-class Passport:
-    fields: Dict[Any, Any] = {}
-
-    def __init__(self, fields):
-        self.fields = fields
-
+def passport_is_valid_p1(fields):
     required_items = {
         "byr",  # (Birth Year)
         "iyr",  # (Issue Year)
@@ -37,119 +31,102 @@ class Passport:
         "pid",  # (Passport ID)
         # "cid",  # (Country ID) NOT REQUIRED
     }
+    return required_items <= set(fields)
 
-    def is_valid_part1(self):
-        return self.required_items <= set(self.fields)
 
-    def is_valid_part2(self):
-        if not self.is_valid_part1():
-            return False
+def passport_is_valid_p2(fields):
+    if not passport_is_valid_p1(fields):
+        return False
 
-        field_objects = [
-            self.FieldByr(self.fields["byr"])
-        ]
-        return all([
-            field.is_valid()
-            for field in field_objects
-        ])
-
-    class FieldNumber:
-        number = 0
-        max_number = 0
-        min_number = 0
-
-        def __init__(self, int_string, min_number, max_number):
-            self.number = int(int_string)
-            self.min_number = min_number
-            self.max_number = max_number
-
-        def is_valid(self):
-            return self.min_number <= self.number <= self.max_number
-
-    class FieldByr(FieldNumber):
-        max_byr = 2002
-        min_byr = 1920
-
-        def __init__(self, byr_string):
-            super().__init__(byr_string, self.min_byr, self.max_byr)
-
-    class FieldIyr(FieldNumber):
-        max_iyr = 2020
-        min_iyr = 2010
-
-        def __init__(self, byr_string):
-            super().__init__(byr_string, self.min_iyr, self.max_iyr)
-
-    class FieldHgt(FieldNumber):
-        max_hgt_cm = 193
-        min_hgt_cm = 150
-        max_hgt_in = 76
-        min_hgt_in = 59
-
-        def __init__(self, hgt_string):
-            if "cm" in hgt_string:
-                super().__init__(
-                    hgt_string.replace("cm", ""),
-                    self.min_hgt_cm,
-                    self.max_hgt_cm
-                )
-            else:
-                super().__init__(
-                    hgt_string.replace("in", ""),
-                    self.min_hgt_in,
-                    self.max_hgt_in
-                )
+    return all([
+        byr_is_valid(fields["byr"]),
+        iyr_is_valid(fields["iyr"]),
+        eyr_is_valid(fields["eyr"]),
+        hgt_is_valid(fields["hgt"]),
+        hcl_is_valid(fields["hcl"]),
+        pid_is_valid(fields["pid"])
+    ])
 
     # a # followed by exactly six characters 0-9 or a-f
-    class FieldHcl:
-        hcl_string = ""
-
-        def __init__(self, hcl_string):
-            self.hcl_string = hcl_string
-
-        def is_valid(self):
-            return len(self.hcl_string) == 7 \
-                    and self.hcl_string[0] == "#" \
-                    and all([
-                        c in "abcdef" + string.digits
-                        for c in self.hcl_string[1:]
-                    ])
-
-    class FieldEcl:
-        eye_colors = [
-            "amb",
-            "blu",
-            "brn",
-            "gry",
-            "grn",
-            "hzl",
-            "oth"
-        ]
-        _ecl = ""
-        def __init__(self, ecl_string):
-            self._ecl = ecl_string
-
-        def is_valid(self):
-            return self._ecl in self.eye_colors
 
 
-def part1():
-    count = sum([
-        passport.is_valid_part1()
-        for passport in parse_passports("../inputs/day4.txt")
+def byr_is_valid(byr_string):
+    min_byr = 1920
+    max_byr = 2002
+    return min_byr <= int(byr_string) <= max_byr
+
+
+def iyr_is_valid(iyr_string):
+    min_iyr = 2010
+    max_iyr = 2020
+    return min_iyr <= int(iyr_string) <= max_iyr
+
+
+def eyr_is_valid(eyr_string):
+    min_eyr = 2020
+    max_eyr = 2030
+    return min_eyr <= int(eyr_string) <= max_eyr
+
+
+def hgt_is_valid(hgt_string):
+    min_hgt_cm = 150
+    max_hgt_cm = 193
+    min_hgt_in = 59
+    max_hgt_in = 76
+    if "cm" in hgt_string:
+        hgt = int(hgt_string.replace("cm", ""))
+        return min_hgt_cm <= hgt <= max_hgt_cm
+    elif "in" in hgt_string:
+        hgt = int(hgt_string.replace("in", ""))
+        return min_hgt_in <= hgt <= max_hgt_in
+    else:
+        return False
+
+
+def hcl_is_valid(hcl_string):
+    return len(hcl_string) == 7 \
+           and hcl_string[0] == "#" \
+           and all([
+        c in "abcdef" + string.digits
+        for c in hcl_string[1:]
     ])
-    print(count)
 
 
-def part2():
-    count = sum([
-        passport.is_valid_part2()
-        for passport in parse_passports("../inputs/day4.txt")
+def ecl_is_valid(ecl):
+    eye_colors = [
+        "amb",
+        "blu",
+        "brn",
+        "gry",
+        "grn",
+        "hzl",
+        "oth"
+    ]
+    for e in eye_colors:
+        if e == ecl:
+            return True
+
+
+def pid_is_valid(pid_string):
+    return len(pid_string) == 9 \
+           and all([c in string.digits for c in pid_string])
+
+
+def part1(filename):
+    return sum([
+        passport_is_valid_p1(passport)
+        for passport in parse_passports(filename)
     ])
-    print(count)
+
+
+def part2(filename):
+    return sum([
+        passport_is_valid_p2(passport)
+        for passport in parse_passports(filename)
+    ])
 
 
 if __name__ == '__main__':
     # 208
-    part1()
-    part2()
+    print(part1("../inputs/day4.txt"))
+    print(part2("../inputs/day4.txt"))
--- python/day4_test.py
+++ python/day4_test.py
@@ -1,5 +1,5 @@
 import unittest
-import day4
+from day4 import *
 
 
 class Day4Tests(unittest.TestCase):
@@ -17,48 +17,33 @@ class Day4Tests(unittest.TestCase):
         for key in self.test_passport:
             bad_passport = self.test_passport.copy()
             bad_passport.pop(key)
-            self.assertFalse(day4.Passport(bad_passport).is_valid_part1())
+            self.assertFalse(passport_is_valid_p1(bad_passport))
 
-        self.assertTrue(day4.Passport(self.test_passport).is_valid_part1())
+        self.assertTrue(passport_is_valid_p1(self.test_passport))
 
-    def test_field_number(self):
-        self.assertFalse(
-            day4.Passport.FieldNumber(0, 1, 2).is_valid()
-        )
-        self.assertFalse(
-            day4.Passport.FieldNumber(3, 1, 2).is_valid()
-        )
-        self.assertTrue(
-            day4.Passport.FieldNumber(1, 1, 2).is_valid()
-        )
-        self.assertTrue(
-            day4.Passport.FieldNumber(2, 1, 2).is_valid()
-        )
+    def test_birth_year(self):
+        self.assertFalse(byr_is_valid(2003))
+        self.assertTrue(byr_is_valid(2000))
+
+    def test_issue_year(self):
+        self.assertFalse(iyr_is_valid(2009))
+        self.assertTrue(iyr_is_valid(2015))
+
+    def test_expire_year(self):
+        self.assertFalse(eyr_is_valid(2019))
+        self.assertTrue(eyr_is_valid(2030))
 
     def test_field_height(self):
-        self.assertFalse(
-            day4.Passport.FieldHgt("160in").is_valid()
-        )
-        self.assertTrue(
-            day4.Passport.FieldHgt("160cm").is_valid()
-        )
-        self.assertTrue(
-            day4.Passport.FieldHgt("60in").is_valid()
-        )
-        self.assertFalse(
-            day4.Passport.FieldHgt("60cm").is_valid()
-        )
+        self.assertFalse(hgt_is_valid("160in"))
+        self.assertTrue(hgt_is_valid("160cm"))
+        self.assertTrue(hgt_is_valid("60in"))
+        self.assertFalse(hgt_is_valid("60cm"))
+        self.assertFalse(hgt_is_valid("60"))
 
     def test_field_hair_color(self):
-        self.assertTrue(
-            day4.Passport.FieldHcl("#123abc").is_valid()
-        )
-        self.assertFalse(
-            day4.Passport.FieldHcl("#123abz").is_valid()
-        )
-        self.assertFalse(
-            day4.Passport.FieldHcl("123abc").is_valid()
-        )
+        self.assertTrue(hcl_is_valid("#123abc"))
+        self.assertFalse(hcl_is_valid("#123abz"))
+        self.assertFalse(hcl_is_valid("123abc"))
 
     def test_field_eye_color(self):
         eye_colors = [
@@ -71,14 +56,32 @@ class Day4Tests(unittest.TestCase):
             "oth"
         ]
         for color in eye_colors:
-            self.assertTrue(
-                day4.Passport.FieldEcl(color).is_valid()
-            )
+            self.assertTrue(ecl_is_valid(color))
+        self.assertFalse(ecl_is_valid("foo"))
+
+    def test_field_pid(self):
+        # their test cases
+        self.assertTrue(pid_is_valid("000000001"))
+        self.assertFalse(pid_is_valid("0123456789"))
+        # added test cases
+        self.assertFalse(pid_is_valid("foobar123"))
+        self.assertFalse(pid_is_valid("012345"))
 
     def test_passport_is_valid_part2(self):
-        return
-        for passport in day4.parse_passports("../inputs/day4_invalid_passports.txt"):
-            self.assertFalse(passport.is_valid_part2(), "passport invalid" + str(passport.fields))
+        for passport in parse_passports("../inputs/day4_invalid_passports.txt"):
+            self.assertFalse(passport_is_valid_p2(passport), "passport invalid" + str(passport))
+
+    def test_part1_example(self):
+        self.assertEqual(2, part1("../inputs/day4_example.txt"))
+
+    def test_part1(self):
+        self.assertEqual(208, part1("../inputs/day4.txt"))
+
+    def test_part2_example(self):
+        self.assertEqual(2, part2("../inputs/day4_example.txt"))
+
+    def test_part2(self):
+        self.assertEqual(222, part2("../inputs/day4.txt"))
 
 
 if __name__ == '__main__':
