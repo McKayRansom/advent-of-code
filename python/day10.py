a1b39856a1a3de1e1622dff92c8c0cfd2ebd67bc
McKay Ransom <mckay.ransom@gmail.com>
2021-01-30 15:49:44 -0700

    tried starting from completed and removing, still too slow :(

--- python/day10.py
+++ python/day10.py
@@ -24,23 +24,23 @@ def part1(filename):
 def part2(filename):
     adapters = parse_number_file(filename)
     adapters.sort()
+    adapters.insert(0, 0)
+    adapters.append(max(adapters) + 3)
 
-    combinations = [[0]]
-    for jolts_rating in adapters:
-        new_combs = []
-        for comb in combinations:
-            max_jolts = comb[-1]
-            if jolts_rating - max_jolts <= 3:
-                new_combs.append(comb)
-                new_combs.append(comb + [jolts_rating])
-        combinations = new_combs
-
-    # must get all the way to max jolts to adapt to device
-    max_jolts = max(adapters)
-    combinations = [
-        comb
-        for comb in combinations
-        if comb[-1] == max_jolts
-    ]
+    # new idea: start from finished list and try and remove adapters
+    combinations = [adapters]
+    for comb in combinations:
+        prev_diff = comb[1] - comb[0]
+        prev_val = comb[1]
+        for i in range(2, len(comb) - 1):
+            # if we can remove the previous entry, do it
+            val = comb[i]
+            diff = val - prev_val
+            if prev_diff + diff <= 3:
+                new_comb = comb[i - 1:]
+                new_comb[0] -= prev_diff
+                combinations.append(new_comb)
+            prev_diff = diff
+            prev_val = val
 
     return len(combinations)
